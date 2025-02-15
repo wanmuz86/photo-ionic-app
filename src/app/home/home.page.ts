69ee72e68950460c5ed9c01c604831e08545c54e
Wan Muzaffar <wanmuz86@gmail.com>
2025-02-15 12:35:38 +0800

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton } from '@ionic/angular/standalone';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,IonButton,CommonModule],
 })
 export class HomePage {
   constructor() {}
+
+  imageUrl: string |undefined;
+
+  async takePicture(){
+    const image = await Camera.getPhoto
+    ({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri, // Keluarkan URL gambar
+      source: CameraSource.Camera
+    });
+
+    this.imageUrl = image.webPath;
+  }
+
+  async selectImage(){
+    const image = await Camera.getPhoto
+    ({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri, // Keluarkan URL gambar
+      source: CameraSource.Photos
+    });
+
+    this.imageUrl = image.webPath;
+  }
 }
