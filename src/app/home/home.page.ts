1ab1ecc73f110813c036ac7bc0c0f7cb10bd51e0
Wan Muzaffar <wanmuz86@gmail.com>
2025-02-15 14:54:09 +0800

    add location

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent,IonButton } from '@ionic/angular/standalone';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { CommonModule } from '@angular/common';
+import { Geolocation } from '@capacitor/geolocation';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -12,6 +14,8 @@ export class HomePage {
   constructor() {}
 
   imageUrl: string |undefined;
+  currentCoordinates: { latitude: number; longitude: number } | null = null;
+
 
   async takePicture(){
     const image = await Camera.getPhoto
@@ -35,5 +39,14 @@ export class HomePage {
     });
 
     this.imageUrl = image.webPath;
+  }
+
+  async getCurrentLocation(){
+  try {
+    const position = await Geolocation.getCurrentPosition();
+    this.currentCoordinates = {latitude: position.coords.latitude, longitude: position.coords.longitude};
+  } catch (error){
+    console.error('Error getting current location', error);
+  }
   }
 }
